e5a31550d5351dd66041de8f7afdcc663766bbe8
Dimitrije Ostojic <dimitrijeo@invaryant.com>
2026-05-22 15:03:46 +0200

    add project references

---
+++ Application/DependecyInjection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependecyInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+    }
+}
